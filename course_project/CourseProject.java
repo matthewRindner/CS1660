b2d6b8154e71afaf2589adcedef0fb7d4c4873c4
Matthew Rindner <32372767+matthewRindner@users.noreply.github.com>
2021-10-31 23:49:25 -0400

    Add files via upload

---
+++ course_project/CourseProject.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class CourseProject {
+
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);  
+
+		System.out.println("Enter Text");
+
+		String str= input.nextLine();
+
+		System.out.print("You have entered: "+str);             
+
+	}
+}
